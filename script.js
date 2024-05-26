543b118e9aab305f92791afcf1db77d59e220ad1
srii03 <162790549+srii03@users.noreply.github.com>
2024-05-26 11:43:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -18,32 +18,38 @@ const clickMe = () => {
 };
 
 const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    let formData = {
+        first_name: $('#first_name').val(),
+        last_name: $('#last_name').val(),
+        password: $('#password').val(),
+        email: $('#email').val()
+    };
     console.log("Form Data Submitted: ", formData);
     // Additional logic to process form data if needed
 };
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = `<div class="col s4 center-align">
-            <div class="card medium">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator materialboxed materialboxed-image" src="${item.image}">
+        let itemToAppend = `
+            <div class="col s4 center-align">
+                <div class="card medium">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator materialboxed" src="${item.image}">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">
+                            ${item.title}<i class="material-icons right">more_vert</i>
+                        </span>
+                        <p><a href="#">${item.link}</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">
+                            ${item.title}<i class="material-icons right">close</i>
+                        </span>
+                        <p>${item.description}</p>
+                    </div>
                 </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
-                    <p><a href="#">${item.link}</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
-                    <p class="card-text">${item.description}</p>
-                </div>
-            </div>
-        </div>`;
+            </div>`;
         $("#card-section").append(itemToAppend);
     });
     // Initialize Materialize materialboxed for newly added images
@@ -51,9 +57,7 @@ const addCards = (items) => {
 };
 
 $(document).ready(function () {
-    $('#formSubmit').click(() => {
-        submitForm();
-    });
+    $('#formSubmit').click(submitForm);
     addCards(cardList);
     $('.modal').modal();
 });
